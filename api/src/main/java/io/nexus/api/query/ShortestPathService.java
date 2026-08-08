0e1534ac0ac5489c5e72676ca379475c6eaf2bfb
jonny5203 <jonnyanhvu@gmail.com>
2026-08-08 02:29:41 +0200

    refactor(api): remove obsolete graph read scaffolding

--- api/src/main/java/io/nexus/api/query/ShortestPathService.java
+++
@@ -1,9 +0,0 @@
-package io.nexus.api.query;
-
-import org.springframework.stereotype.Service;
-
-/** Raw Cypher shortest-path via Neo4jClient (PLAN.md T7.4). */
-@Service
-public class ShortestPathService {
-    // TODO(T7.4): MATCH p = shortestPath((a)--(b)) ... via Neo4jClient.unboundRunnable().cypher(...)
-}
